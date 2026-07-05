471ef7736e554010c4f35dc8e346db525d21a951
Dejah Cooper <134736497+dejahcooper@users.noreply.github.com>
2026-07-05 00:08:23 -0400

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let mapped = {}
+
+        for (let num of nums) {
+            if (mapped[num]) {
+                return true
+            } else {
+                mapped[num] = 1
+            }
+        }
+        return false
+    }
+}
